5dfa8ca071ec81d4b50a238ed98ed67744add145
Matus Kral <matuskral@me.com>
2015-02-27 15:54:37 +0100

    Stopping XBMC from rendering the screen if not needed (TV off, or XBMC not selected as root). On reactivation, it also updates monitor/TV resolutions and adapts screen output if needed.
    Upstream-commit: 0bf5cbfc52c0ac503d4f6cab2e9a6d709fca740f

--- xbmc/Application.h
+++ xbmc/Application.h
@@ -79,6 +79,8 @@ namespace PLAYLIST
 #include "ApplicationPlayer.h"
 #include "FileItem.h"
 
+#include "guilib/Resolution.h"
+
 class CSeekHandler;
 class CInertialScrollingHandler;
 class DPMSSupport;
@@ -308,6 +310,7 @@ public:
   inline bool IsInScreenSaver() { return m_bScreenSave; };
   inline bool IsDPMSActive() { return m_dpmsIsActive; };
   int m_iScreenSaveLock; // spiff: are we checking for a lock? if so, ignore the screensaver state, if -1 we have failed to input locks
+  RESOLUTION_INFO m_res;
 
   bool m_bPlaybackStarting;
   typedef enum
@@ -404,6 +407,10 @@ public:
   */
   void UnlockFrameMoveGuard();
 
+  bool ScreenSaverDisablesAutoScrolling();
+  void SetCecStandby(bool status);
+  bool GetCecStandby() { return m_cecStandby; }
+
 protected:
   virtual bool OnSettingsSaving() const override;
 
@@ -428,6 +435,8 @@ protected:
 
   bool m_saveSkinOnUnloading;
   bool m_autoExecScriptExecuted;
+
+  bool m_cecStandby;
 
 #if defined(TARGET_DARWIN_IOS)
   friend class CWinEventsIOS;
