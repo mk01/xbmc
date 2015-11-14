5b366cf37766514d84dd05ab7fd94d70bbc03354
Matus Kral <matuskral@me.com>
2015-11-14 22:34:17 +0100

    [CNetwork] - remove IsAvailable() completely
    - fix calls to it from other layers either by calling IsConnected()
    instead, or remove the call completely if had no practical meaning
    - don't call services start() if we have no interfaces configured at all
    (at least loopback is needed)

--- xbmc/windows/GUIMediaWindow.cpp
+++ xbmc/windows/GUIMediaWindow.cpp
@@ -1639,7 +1639,7 @@ const CFileItemList& CGUIMediaWindow::CurrentDirectory() const
 
 bool CGUIMediaWindow::WaitForNetwork() const
 {
-  if (g_application.getNetwork().IsAvailable())
+  if (g_application.getNetwork().IsConnected())
     return true;
 
   CGUIDialogProgress *progress = (CGUIDialogProgress *)g_windowManager.GetWindow(WINDOW_DIALOG_PROGRESS);
@@ -1651,7 +1651,7 @@ bool CGUIMediaWindow::WaitForNetwork() const
   progress->SetLine(1, CVariant{url.GetWithoutUserDetails()});
   progress->ShowProgressBar(false);
   progress->Open();
-  while (!g_application.getNetwork().IsAvailable())
+  while (!g_application.getNetwork().IsConnected())
   {
     progress->Progress();
     if (progress->IsCanceled())
