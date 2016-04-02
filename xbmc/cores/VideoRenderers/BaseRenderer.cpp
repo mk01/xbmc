xbianonpi/xbian-sources-xbmc

Fix crash on start if SDTV is used

xbmc/cores/VideoRenderers/BaseRenderer.cpp
@@ -228,6 +228,9 @@ void CBaseRenderer::FindResolutionFromFpsMatch(float fps, float& weight)
 
 RESOLUTION CBaseRenderer::FindClosestResolution(float fps, float multiplier, RESOLUTION current, float& weight)
 {
+  if (CDisplaySettings::Get().ResolutionInfoSize() < 2)
+    return current;
+
   RESOLUTION_INFO curr = g_graphicsContext.GetResInfo(current);
 
   float fRefreshRate = fps;

mk01/xbmc

(cherry picked from commit eb1effb52f3985e86e567493d86fa7fe2573920a)
Signed-off-by: Matus Kral <matuskral@me.com>

xbmc/cores/VideoRenderers/BaseRenderer.cpp
@@ -229,6 +229,9 @@ void CBaseRenderer::FindResolutionFromFpsMatch(float fps, float& weight)
 
 RESOLUTION CBaseRenderer::FindClosestResolution(float fps, float multiplier, RESOLUTION current, float& weight)
 {
+  if (CDisplaySettings::GetInstance().ResolutionInfoSize() < 2)
+    return current;
+
   RESOLUTION_INFO curr = g_graphicsContext.GetResInfo(current);
   RESOLUTION orig_res  = CDisplaySettings::GetInstance().GetCurrentResolution();
 