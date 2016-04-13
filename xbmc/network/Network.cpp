xbianonpi/xbian-sources-xbmc

CNetwork: make restart of services on network change user setting dependent

xbmc/network/Network.cpp
@@ -22,6 +22,7 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
+#include "settings/Settings.h"
 #include "Network.h"
 #include "ApplicationMessenger.h"
 #include "network/NetworkServices.h"
@@ -422,8 +423,11 @@ void CNetwork::NetworkMessage(EMESSAGE message, int param)
       m_signalNetworkChange.Set();
       CLog::Log(LOGDEBUG, "%s - Network setup changed. Will restart network services",__FUNCTION__);
       ANNOUNCEMENT::CAnnouncementManager::Get().Announce(ANNOUNCEMENT::Network, "network", "OnInterfacesChange");
-      NetworkMessage(SERVICES_DOWN, 0);
-      NetworkMessage(SERVICES_UP, 0);
+      if (CSettings::Get().GetBool("network.restartservices"))
+      {
+        NetworkMessage(SERVICES_DOWN, 0);
+        NetworkMessage(SERVICES_UP, 0);
+      }
       break;
   }
 }

mk01/xbmc

Upstream-commit: 3fc33181ee149c12b9efe193fc76c9855431be45

xbmc/network/Network.cpp
@@ -24,6 +24,7 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
+#include "settings/Settings.h"
 #include "Network.h"
 #include "messaging/ApplicationMessenger.h"
 #include "network/NetworkServices.h"
@@ -417,10 +418,13 @@ void CNetwork::NetworkMessage(EMESSAGE message, int param)
 
     case NETWORK_CHANGED:
       m_signalNetworkChange.Set();
-      CLog::Log(LOGDEBUG, "%s - Network setup changed. Will restart network services",__FUNCTION__);
       ANNOUNCEMENT::CAnnouncementManager::GetInstance().Announce(ANNOUNCEMENT::Network, "network", "OnInterfacesChange");
-      NetworkMessage(SERVICES_DOWN, 0);
-      NetworkMessage(SERVICES_UP, 0);
+      if (CSettings::GetInstance().GetBool("network.restartservices"))
+      {
+        CLog::Log(LOGDEBUG, "%s - Network setup changed. Will restart network services",__FUNCTION__);
+        NetworkMessage(SERVICES_DOWN, 0);
+        NetworkMessage(SERVICES_UP, 0);
+      }
       break;
   }
 }